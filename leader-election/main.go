db8fb3ce253163a36f369329a2811b8cd9f49fbe
JayJamieson <jacques@jamieson.kiwi>
2022-07-26 22:50:06 +1200

    added session creation and reading for existing sessions

--- leader-election/main.go
+++ leader-election/main.go
@@ -1,6 +1,16 @@
 package main
 
-import "github.com/hashicorp/consul/api"
+import (
+	"io/ioutil"
+	"log"
+
+	"github.com/hashicorp/consul/api"
+)
+
+func getSession() (string, error) {
+	sessionId, err := ioutil.ReadFile("session.id")
+	return string(sessionId), err
+}
 
 func main() {
 	// Get a new client
@@ -11,7 +21,30 @@ func main() {
 
 	session := client.Session()
 
-	session.CreateNoChecks(&api.SessionEntry{
-		Name: "worker1",
-	}, &api.WriteOptions{})
+	existingSessionId, err := getSession()
+
+	var sessionId = existingSessionId
+
+	if err != nil {
+		log.Println("No existing session found")
+
+		sessionId, _, err = session.CreateNoChecks(&api.SessionEntry{
+			Name: "worker1",
+		}, &api.WriteOptions{})
+
+		if err != nil {
+			panic(err)
+		}
+
+		log.Println("Created new session in session.id file")
+		ioutil.WriteFile("session.id", []byte(sessionId), 0644)
+	}
+
+	log.Printf("Session ID %v", sessionId)
+
+	// get a lock on the key using session
+	// client.KV().Acquire()
+
+	// renew the session to keep the lock active
+	// session.RenewPeriodic()
 }
