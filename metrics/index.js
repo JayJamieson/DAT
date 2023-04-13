6d84906645da8860b2b3f793f6c26c1190d8f2f4
jay <jacques.jamieson@fergusapp.com>
2023-04-13 16:45:41 +1200

    added sample error log data for metric filter creation

--- metrics/index.js
+++ metrics/index.js
@@ -1,165 +1,110 @@
 import { CloudWatchClient, PutMetricDataCommand } from "@aws-sdk/client-cloudwatch";
+import { CloudWatchLogsClient, PutLogEventsCommand, CreateLogStreamCommand } from "@aws-sdk/client-cloudwatch-logs";
+import os from "node:os";
 
 const cloudwatchClient = new CloudWatchClient({region: "ap-southeast-2"});
+const clientwatchLogClient = new CloudWatchLogsClient({region: "ap-southeast-2"});
 
-async function putMetricReece() {
-  const params = {
-  Namespace: "invoice-ingestion-stats-poc",
-  MetricData: [
+async function main() {
+  // await clientwatchLogClient.send(new CreateLogStreamCommand({ // CreateLogStreamRequest
+  //   logGroupName: "/CWAgent/Fergus/Queue/syslog", // required
+  //   logStreamName: os.hostname(), // required
+  // }));
+
+  await seedErrorLogs();
+
+  const dataArray = [
     {
-      MetricName: "Total invoices ingested",
-      Dimensions: [
-        {
-          Name: "InvoiceSource",
-          Value: "manual"
-        },
-        {
-          Name: "Supplier",
-          Value: "Reece"
-        }
-      ],
-      Timestamp: new Date(),
-      Value: 10,
-      Unit: "Count"
+      supplierName: "Reece",
+      invoiceSource: "drag_and_drop",
+      value: 30,
+      timestamp: new Date("2023-04-12T12:34:00Z")
     },
     {
-      MetricName: "Total invoices ingested",
-      Dimensions: [
-        {
-          Name: "InvoiceSource",
-          Value: "drag_and_drop"
-        },
-        {
-          Name: "Supplier",
-          Value: "Reece"
-        }
-      ],
-      Timestamp: new Date(),
-      Value: 30,
-      Unit: "Count"
+      supplierName: "Reece",
+      invoiceSource: "email",
+      value: 5,
+      timestamp: new Date("2023-04-13T01:23:00Z")
     },
     {
-      MetricName: "Total invoices ingested",
-      Dimensions: [
-        {
-          Name: "InvoiceSource",
-          Value: "email"
-        },
-        {
-          Name: "Supplier",
-          Value: "Reece"
-        }
-      ],
-      Timestamp: new Date(),
-      Value: 5,
-      Unit: "Count"
-    },
-    {
-      MetricName: "Total invoices ingested",
-      Dimensions: [
-        {
-          Name: "InvoiceSource",
-          Value: "premium_or_unknown_source"
-        },
-        {
-          Name: "Supplier",
-          Value: "Reece"
-        }
-      ],
-      Timestamp: new Date(),
-      Value: 50,
-      Unit: "Count"
+      supplierName: "Reece",
+      invoiceSource: "premium_or_unknown_source",
+      value: 50,
+      timestamp: new Date("2023-04-13T01:23:00Z")
     }
-  ]
-};
-  try {
+    ];
 
-    const data = await cloudwatchClient.send(new PutMetricDataCommand(params));
-    return data;
-  } catch (err) {
-    console.log("Error sending metric:", err);
-  }
+    putMetric(dataArray);
 }
 
-async function putMetricMico() {
-  const params = {
-  Namespace: "invoice-ingestion-stats-poc",
-  MetricData: [
-    {
-      MetricName: "Total invoices ingested",
-      Dimensions: [
-        {
-          Name: "InvoiceSource",
-          Value: "manual"
-        },
-        {
-          Name: "Supplier",
-          Value: "Mico"
-        }
-      ],
-      Timestamp: new Date(),
-      Value: 11,
-      Unit: "Count"
-    },
-    {
-      MetricName: "Total invoices ingested",
-      Dimensions: [
-        {
-          Name: "InvoiceSource",
-          Value: "drag_and_drop"
-        },
-        {
-          Name: "Supplier",
-          Value: "Mico"
-        }
-      ],
-      Timestamp: new Date(),
-      Value: 33,
-      Unit: "Count"
-    },
-    {
-      MetricName: "Total invoices ingested",
-      Dimensions: [
-        {
-          Name: "InvoiceSource",
-          Value: "email"
-        },
-        {
-          Name: "Supplier",
-          Value: "Mico"
-        }
-      ],
-      Timestamp: new Date(),
-      Value: 4,
-      Unit: "Count"
-    },
-    {
-      MetricName: "Total invoices ingested",
-      Dimensions: [
-        {
-          Name: "InvoiceSource",
-          Value: "premium_or_unknown_source"
-        },
-        {
-          Name: "Supplier",
-          Value: "Mico"
-        }
-      ],
-      Timestamp: new Date(),
-      Value: 1,
-      Unit: "Count"
-    }
-  ]
-};
-  try {
+main()
 
-    const data = await cloudwatchClient.send(new PutMetricDataCommand(params));
-    return data;
-  } catch (err) {
-    console.log("Error sending metric:", err);
+async function seedErrorLogs() {
+  const rand = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
   }
+
+  const supplierNames = ["Reece", "Mico", "JA Russel", "Tradelink"];
+
+  const input = {
+    // PutLogEventsRequest
+    logGroupName: "/CWAgent/Fergus/Queue/syslog", // required
+    logStreamName: os.hostname(), // required
+    logEvents: [],
+  };
+
+  for (let i = 0; i <= 100; i++) {
+    const id = rand(0, supplierNames.length-1);
+    const batch = rand(0, 100);
+    const event = {
+      timestamp: Date.now(), // required
+      message: JSON.stringify({
+        message: "INVOICE_IMPORT_FAILURE", // required
+        context: {
+          supplier_id: id,
+          supplier_name: supplierNames[id],
+          company_id: id,
+          file: {
+            file_id: 1,
+            file_size: 1,
+            s3_path: "/tmp",
+          },
+          exception: "Some exception",
+          batch_id: batch,
+        },
+      }),
+    }
+
+    input.logEvents.push(event);
+  }
+
+  const command = new PutLogEventsCommand(input);
+
+  await clientwatchLogClient.send(command);
 }
 
-putMetricReece();
+async function putMetric(dataArray) {
 
-putMetricMico();
+  const params = {
+    Namespace: "invoice-ingestion-stats-poc",
+    MetricData: dataArray.map((data) => ({
+      MetricName: "Total invoices ingested",
+      Dimensions: [
+        {
+          Name: "InvoiceSource",
+          Value: data.invoiceSource
+        },
+        {
+          Name: "Supplier",
+          Value:  data.supplierName
+        }
+      ],
+      Timestamp: data.timestamp,
+      Value: data.value,
+      Unit: "Count"
+    }))
+  };
+  await cloudwatchClient.send(new PutMetricDataCommand(params));
+}
